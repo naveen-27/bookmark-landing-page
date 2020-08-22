d8832673449453ba37de28fe928e13b639137d17
Naveen Kumar <456endson@gmail.com>
2020-08-22 14:35:04 +0530

    Fixed Links display on resizing

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,5 +1,13 @@
 const NAV_LINKS = document.querySelector('.nav-links');
 
 document.querySelector('img[alt="Expand Menu"]').addEventListener('click', () => {
-    NAV_LINKS.style.display = (NAV_LINKS.style.display === 'none') ? 'flex' : 'none';
+    NAV_LINKS.classList.toggle('hidden');
 });
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 800) {
+        NAV_LINKS.classList.remove('hidden');
+    } else {
+        NAV_LINKS.classList.add('hidden');
+    }
+})
